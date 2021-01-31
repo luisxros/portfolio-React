7a87799c602df33ff54ca0296c12bbee51f4e3db
Luis Rosario <luisalmonter@gmail.com>
2021-01-31 18:08:42 -0500

    adds style to contact and header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,14 +6,17 @@ function Header(props) {
     return(
         <header className="Header">
             <div className="Name">
-                <h1>Luis Rosario</h1>
+                Luis Rosario
+            </div>
+            <div className="Title">
+                Full Stack Software Engineer
             </div>
             <nav>
                 <ul className='NavLinks'>
-                    <li><Link to='#1'>About me</Link></li>
+                    <li><Link to='#1'>About</Link></li>
                     <li><Link to='#2'>Projects</Link></li>
                     <li><Link to='#3'>Skills</Link></li>
-                    <li><Link to='#4'>Contact me</Link></li>
+                    <li><Link to='#4'>Contact</Link></li>
                 </ul>
             </nav>
         </header>
