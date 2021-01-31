3b72eb355513579fefb99f0ef9e59195d43e0235
Luis Rosario <luisalmonter@gmail.com>
2021-01-30 21:14:31 -0500

    adds details to about and skills components

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,17 @@ import './About.css';
 function About(props) {
     return(
         <div className="About" id="1">
-            <h1>About</h1>
+            <h1>About Me</h1>
+            <div className="img">
+                <img src=""/>
+            </div>
+            <div className="info">
+            <p>
+                    I’m a Software Engineer with a background in customer service, and degree in design. I use my
+                    experiences together to make interactions with my clients better, and be able to fulfill their
+                    needs.
+                </p>
+            </div>
         </div>
     );
 }
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,7 +4,12 @@ import './Skills.css'
 function Skills(props) {
     return(
         <div className="Skills" id="3">
-            <h1>Skills</h1>
+            <h1>Software Skills:</h1>
+            <div className="info">
+                <p>
+                    JavaScript, React.js, React Native, Vanilla Javascript, JQuery, HTML5, CSS3, SASS, Ruby, Ruby on Rails, Bootstrap, Styled-Components, Trello, Heroku, PostgreSQL, SQL,  Postman,  Express, Node.js, MongoDB, Mongoose,  Git, GitHub, Microsoft Office(Word, Excel, PowerPoint, etc.) |, SCRUM, Agile 
+            </p>
+            </div>
         </div>
     );
 }
