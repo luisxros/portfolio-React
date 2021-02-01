88421d61620fc5f7335c2629ee37281637cc6f1b
Luis Rosario <luisalmonter@gmail.com>
2021-01-31 19:00:49 -0500

    adds links to images and name

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -11,32 +11,42 @@ function Projects(props) {
             <h1>Projects:</h1>
 
             <div className="Super">
+            <a href="https://super-information.vercel.app/" target="_blank">
                 <h5>Super Information:</h5>
                 <img src={Super} className="project1" alt="Super-Information"/>
+            </a>
                 <p>
                 Web app that displays heroes and villains information using a superhero API
                 </p>
             </div>
 
             <div className="Post">
+             <a href="https://post-it-appx.herokuapp.com/" target="_blank">
                 <h5>Post-It:</h5>
                 <img src={Post} className="project2" alt="Post-it"/>
+            </a>
                 <p>
                 Web application with authentication for the user to share thoughts and comment on existing posts. 
                 </p>
             </div>
 
             <div className="Trending">
-                <h5>Trending:</h5>
-                <img src={Trending} className="project3" alt="Trending"/>
+
+                <a href="https://project-trending.netlify.app/" target="_blank">
+                    <h5>Trending:</h5>
+                    <img src={Trending} className="project3" alt="Trending"/>
+                </a>
                 <p>
                 Web application for users to see the latest trending movies & tv shows built in an  agile sprint
                 </p>
             </div>
 
             <div className="Tune">
-                <h5>Tune-it-up:</h5>
-                <img src={Tune} className="project4" alt="Tune-it-up"/>
+
+                <a href="https://tune-it-up.netlify.app/" target="_blank">
+                    <h5>Tune-it-up:</h5>
+                    <img src={Tune} className="project4" alt="Tune-it-up"/>
+                </a>
                 <p>
                     A Web app that combines a Rails backend with a React front end. A user can use Tune It Up to keep track of and save his, her, or their favorite songs!
                 </p>
